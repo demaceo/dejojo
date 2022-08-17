0bc383d17f1315f5dcebfe528f16c1c2ed8cf4d5
demaceo <hdemaceo@gmail.com>
2022-08-16 18:43:26 -0600

    add functionality to input search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import React, {
-  // Component,
-  // useRef,
-  useState,
-  // useReducer,
-  useEffect,
-  // useContext,
-} from "react";
+import { Router, Route } from "react-router";
+
+import React, { useState, useEffect } from "react";
 import Header from "./Components/Header.js";
 // import Loader from "./Components/Loader.js";
 import InputSearch from "./Components/InputSearch.js";
@@ -15,33 +10,28 @@ import CoinTable from "./Components/CoinTable.js";
 // import Footer from "./Components/Footer.js";
 
 function App() {
-  let [main, setMain] = useState([]);
+  // let [main, setMain] = useState([]);
   // const mainRef = useRef();
 
-  useEffect(() => {
-    fetchMainData();
-    setTimeout(5000)
+  // useEffect(() => {
+  //   fetchMainData();
+  // }, []);
 
-  }, []);
+  // const fetchMainData = () => {
+  //   fetch("https://api.coingecko.com/api/v3/coins/cardano")
+  //     .then((res) => res.json())
+  //     .then((response) => {
+  //       setMain(response);
+  //     })
+  //     .catch((e) => console.log(e));
+  // };
 
-  const fetchMainData = () => {
-    fetch("https://api.coingecko.com/api/v3/coins/cardano")
-      .then((res) => res.json())
-      .then((response) => {
-        setMain(response);
-      })
-      .catch((e) => console.log(e));
-  }
-
-    return (
-      <>
-        <section className="App">
-          <Header/>
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <InputSearch />
-          <CoinTable />
-        </section>
-      </>
-    );
-  }
+  return (
+    <section className="App">
+      <Header />
+      <InputSearch />
+      <CoinTable />
+    </section>
+  );
+}
 export default App;
--- src/Components/InputSearch.js
+++ src/Components/InputSearch.js
@@ -1,28 +1,50 @@
 import "./InputSearch.css";
-import React from "react";
-// import { Container, Row, Alert, Col } from "react-bootstrap";
+import React, { useState, useRef } from "react";
 
 
 function InputSearch() {
-  // const getNewImage = () => {
-  //   let img = React.createElement("img", {
-  //     className: "main-photo",
-  //     src: "https://picsum.photos/1000?" + Math.random(),
-  //     ref: mainRef,
-  //       onClick: movePhoto,
-  //   });
-  //   setMain(img);
-  //   console.log("main", main);
-  //   return img;
-  // };
+  const [coin, setCoin] = useState([]);
+  const searchRef = useRef();
+
+  const searchCoin = async (event) => {
+    event.preventDefault();
+    if (searchRef.current.value) {
+      await fetchData(searchRef.current.value.toLowerCase());
+      // let searchedCoin = React.createElement("section", {
+      //   className: "main-coin",
+      //   id: coin.id,
+      //   // ref: mainRef,
+      //   // onClick: movePhoto,
+      // });
+      searchRef.current.value = "";
+      // return searchedCoin;
+    }
+  };
+
+  const fetchData = (coinid) => {
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinid}`)
+      .then((res) => res.json())
+      .then((response) => {
+        setCoin(response);
+        // console.log("coin", coin);
+      })
+      .catch((e) => console.log(e));
+  };
+
   return (
-    <form action="/cointable">
+    <form action="/">
       <section className="coinSearchSection">
         <input
           className="coinInput"
-          placeholder="Please enter the name of the coin you would like to search..."
+          ref={searchRef}
+          placeholder="   Please enter the name of the coin you would like to search..."
         />
-        <input className="coinButton" type="submit" value="$earch" />
+        <input
+          className="coinButton"
+          type="submit"
+          value="$earch"
+          onClick={searchCoin}
+        />
       </section>
     </form>
   );
